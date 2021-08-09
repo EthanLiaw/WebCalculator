bb9527a96caf48966a8997ed18c33b8257eb6e4f
Ethan Liaw <ethanliaw@eaton.com>
2021-08-09 13:24:22 -0400

    specifying 8080 as port

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
 @app.route("/")
 def landing_page(name=None):
     return render_template('calculator_input.html')
